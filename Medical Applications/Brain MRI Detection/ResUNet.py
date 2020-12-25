7d4a05cc3a0a2ccb3d2dfcfb372dcaa0415c1818
unknown <vesaalexandru95@gmail.com>
2020-12-25 13:00:54 +0200

    Segmentation model added on Kaggle competition

--- Medical Applications/Brain MRI Detection/ResUNet.py
+++ Medical Applications/Brain MRI Detection/ResUNet.py
@@ -17,7 +17,7 @@ from skimage import io
 import tensorflow as tf
 from tensorflow.python.keras import Sequential
 from tensorflow.keras import layers, optimizers
-from tensorflow.keras.layers import *
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Add, Activation, Concatenate, UpSampling2D,Input,InputLayer, MaxPool2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model
 import tensorflow.keras.backend as K
@@ -25,15 +25,35 @@ import tensorflow.keras.backend as K
 import random
 import glob
 from sklearn.preprocessing import StandardScaler, normalize
-from Ipython.display import display
+#from Ipython.display import display
 import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 from dataset import create_dataset
+from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
+from sklearn.metrics import confusion_matrix, classification_report,accuracy_score
 
-
-#Create dataset
-brain_df = create_dataset()
-
+def create_callbacks():
+    earlystopping = EarlyStopping(monitor='val_loss', 
+                              mode='min', 
+                              verbose=1, 
+                              patience=15
+                             )
+    checkpointer = ModelCheckpoint(filepath="clf-resnet-weights.hdf5", 
+                                   verbose=1, 
+                                   save_best_only=True
+                                  )
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                                  mode='min',
+                                  verbose=1,
+                                  patience=10,
+                                  min_delta=0.0001,
+                                  factor=0.2
+                                 )
+    callbacks = [checkpointer, earlystopping, reduce_lr]
+    
+    return callbacks
 
 def pos_neg_diagnosis(mask_path):
     value = np.max(cv2.imread(mask_path))
@@ -44,20 +64,186 @@ def pos_neg_diagnosis(mask_path):
     
 
 
+def get_data():
     
-brain_df['mask'] = brain_df['mask_path'].apply(lambda x : pos_neg_diagnosis(x))
-patients_id = np.unique(brain_df.patient_id)
 
-patients_id_train= patients_id[0:77]
-patients_id_val = patients_id[0:16]
-patients_id_test = patients_id[0:17]
+    #Create dataset
+    brain_df = create_dataset()
+    brain_df['mask'] = brain_df['mask_path'].apply(lambda x : pos_neg_diagnosis(x))
+
+    #Search for the mask    
+    brain_df_mask = brain_df[brain_df['mask'] == 1]
+
+    #Creating train, val , test
+    
+    x_train, x_val = train_test_split(brain_df_mask, test_size = 0.15)
+    x_test, x_val =  train_test_split(x_val, test_size = 0.5)
+    
+    train_ids = list(x_train.image_path)
+    train_mask = list(x_train.mask_path)
+    
+    val_ids = list(x_val.image_path)
+    val_mask = list(x_val.mask_path)
+    
+    return train_ids, train_mask, val_ids, val_mask
 
 
-train_df = brain_df[brain_df['patient_id'].isin(patients_id_train) ]
-val_df = brain_df[brain_df['patient_id'].isin(patients_id_val) ]
-test_df = brain_df[brain_df['patient_id'].isin(patients_id_test) ]
-#Creating Test, Train, Val Set
-brain_df_train = brain_df.drop(columns = 'patient_id')
-brain_df_train['mask'] = brain_df_train['mask'].apply(lambda x: str(x))
+class DataGeneratorSegmentation(tf.keras.utils.Sequence):
+    def __init__(self, ids, mask, image_dir = './', batch_size = 16, img_h=256, img_w=256, shuffle=True):
+        
+        self.ids = ids
+        self.mask = mask
+        self.image_dir = image_dir
+        self.batch_size = batch_size
+        self.img_h = img_h
+        self.img_w = img_w
+        self.shuffle = shuffle
+        self.on_epoch_end()
+        
+    def __len__(self):
+        return int(np.floor(len(self.ids)) / self.batch_size)
+    
+    def __getitem__(self, index):
+        
+        #generate index of batch_size length
+        indexes = self.indexes[index*self.batch_size : (index+1)*self.batch_size]
+        
+        list_ids = [self.ids[i] for i in indexes]
+        list_mask = [self.mask[i] for i in indexes]
+        
+        X, y = self.__data_generation(list_ids, list_mask)
+        
+        return X,y
 
-train, test = train_test_split(brain_df_train, test_size=0.15)
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.ids))
+        
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_ids, list_mask):
+        
+        X = np.empty((self.batch_size, self.img_h, self.img_w, 3))
+        y = np.empty((self.batch_size, self.img_h, self.img_w, 1))
+        
+        for i in range(len(list_ids)):
+            img_path =  str(list_ids[i])
+            
+            mask_path = str(list_mask[i])
+            
+            img = io.imread(img_path)
+            mask = io.imread(mask_path)
+            
+            img = cv2.resize(img, (self.img_h, self.img_w))
+            img = np.array(img, dtype = np.float64)
+            
+            mask = cv2.resize(mask, (self.img_h, self.img_w))
+            mask = np.array(mask, dtype = np.float64)
+            
+            #Standardising
+            img -= img.mean()
+            img /=img.std()
+            
+            mask -= mask.mean()
+            mask /=mask.std()
+            
+            #Add image to empty array
+            X[i,] = img
+            y[i,] = np.expand_dims(mask, axis=2)
+            
+        #Normalize y
+        y = (y>0).astype(int)
+        
+        return X,y
+
+class ResUnet:
+    
+    def __init__(self,input_shape = (256,256,3)):
+        
+        self.input_shape = input_shape
+        self.model = self.build()
+    
+    def resblock(self, X,filters):
+        X_copy = X
+        
+        X = Conv2D(filters, kernel_size = (1,1), kernel_initializer = 'he_normal')(X)
+        X = BatchNormalization()(X)
+        X = Activation('relu')(X)
+        
+        X = Conv2D(filters, kernel_size = (3,3),padding='same',kernel_initializer = 'he_normal')(X)
+        X = BatchNormalization()(X)
+        
+        #Shortcut path
+        X_copy = Conv2D(filters, kernel_size = (1,1), kernel_initializer = 'he_normal')(X_copy)
+        X_copy = BatchNormalization()(X_copy)
+        
+        X = Add()([X, X_copy])
+        X = Activation('relu')(X)
+        
+    def upsample_concat(self, x, skip):
+        X = UpSampling2D((2,2))(x)
+        merge = Concatenate()([X, skip])
+        
+        return merge
+    
+    def build(self):
+        
+        input_layer = Input(shape =(256,256,3))
+        
+        #Stage1
+        conv_1 = Conv2D(16,3, activation = 'relu', padding='same', kernel_initializer='he_normal')(input_layer)
+        conv_1 = BatchNormalization()(conv_1)
+        conv_1 = Conv2D(16,3, activation = 'relu', padding='same', kernel_initializer='he_normal')
+        conv_1 = BatchNormalization()(conv_1)  
+        pool_1 = MaxPool2D((2,2))(conv_1)
+        
+        #Stage2
+        conv_2 = self.resblock(pool_1, 32)
+        pool_2 = MaxPool2D((2,2))(conv_2)
+        
+        #Stage3
+        conv_3 = self.resblock(pool_2, 64)
+        pool_3 = MaxPool2D((2,2))(conv_3)
+        
+        #Stage4
+        conv_4 = self.resblock(pool_3, 128)
+        pool_4 = MaxPool2D((2,2))(conv_4)
+        
+        #Stage5
+        conv_5 = self.resblock(pool_4, 256)
+        
+        # Upsample Stage 1
+        up_1 = self.upsample_concat(conv_5, conv_4)
+        up_1 = self.resblock(up_1, 128)
+        
+        # Upsample Stage 2
+        up_2 = self.upsample_concat(up_1, conv_3)
+        up_2 = self.resblock(up_2, 64)
+        
+        # Upsample Stage 3
+        up_3 = self.upsample_concat(up_2, conv_2)
+        up_3 = self.resblock(up_3, 32)
+        
+        # Upsample Stage 4
+        up_4 = self.upsample_concat(up_3, conv_1)
+        up_4 = self.resblock(up_4, 16)
+        
+        # final output
+        out = Conv2D(1, (1,1), kernel_initializer='he_normal', padding='same', activation='sigmoid')(up_4)
+        
+        seg_model = Model(input_layer, out)
+        seg_model.summary()
+        
+        return seg_model
+
+
+def get_generators():    
+
+    train_ids, train_mask, val_ids, val_mask = get_data()
+    train_data = DataGeneratorSegmentation(train_ids, train_mask )
+    val_data = DataGeneratorSegmentation(val_ids, val_mask)
+    
+    return train_data, val_data
+
+
+model = ResUnet()
---
+++ Medical Applications/Brain MRI Detection/classification_model.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 23:24:50 2020
+
+@author: 40737
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 16:11:47 2020
+
+@author: 40737
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import zipfile
+import cv2
+from skimage import io
+
+import tensorflow as tf
+from tensorflow.python.keras import Sequential
+from tensorflow.keras import layers, optimizers
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
+import tensorflow.keras.backend as K
+
+import random
+import glob
+from sklearn.preprocessing import StandardScaler, normalize
+#from Ipython.display import display
+import plotly.graph_objects as go
+from sklearn.model_selection import train_test_split
+from dataset import create_dataset
+from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
+from sklearn.metrics import confusion_matrix, classification_report,accuracy_score
+#Create dataset
+brain_df = create_dataset()
+
+
+def pos_neg_diagnosis(mask_path):
+    value = np.max(cv2.imread(mask_path))
+    if value > 0 :
+        return 1
+    else:
+        return 0
+    
+
+
+    
+brain_df['mask'] = brain_df['mask_path'].apply(lambda x : pos_neg_diagnosis(x))
+
+#Create unique dataset without same id pacient in any of dataset
+patients_id = np.unique(brain_df.patient_id)
+
+patients_id_train= patients_id[0:77]
+patients_id_val = patients_id[0:16]
+patients_id_test = patients_id[0:17]
+
+
+train_df = brain_df[brain_df['patient_id'].isin(patients_id_train) ]
+val_df = brain_df[brain_df['patient_id'].isin(patients_id_val) ]
+test_df = brain_df[brain_df['patient_id'].isin(patients_id_test) ]
+
+train_df = train_df.drop(columns = 'patient_id')
+val_df = val_df.drop(columns = 'patient_id')
+test_df = test_df.drop(columns = 'patient_id')
+
+train_df['mask'] =train_df['mask'].apply(lambda x: str(x))
+val_df['mask'] =val_df['mask'].apply(lambda x: str(x))
+test_df['mask'] =test_df['mask'].apply(lambda x: str(x))
+
+datagen = ImageDataGenerator(rescale = 1./255 )
+
+train_generator = datagen.flow_from_dataframe(train_df,
+                                              directory = '/',
+                                              x_col = 'image_path',
+                                              y_col = 'mask',
+                                              subset = 'training',
+                                              class_mode = 'categorical',
+                                              batch_size = 16,
+                                              shuffle=True,
+                                              target_size = (256,256))
+
+val_generator = datagen.flow_from_dataframe(val_df,
+                                              directory = '/',
+                                              x_col = 'image_path',
+                                              y_col = 'mask',                                            
+                                              class_mode = 'categorical',
+                                              batch_size = 16,
+                                              shuffle=True,
+                                              target_size = (256,256))
+
+
+test_generator = datagen.flow_from_dataframe(test_df,
+                                              directory = '/',
+                                              x_col = 'image_path',
+                                              y_col = 'mask',                                            
+                                              class_mode = 'categorical',
+                                              batch_size = 16,
+                                              shuffle=True,
+                                              target_size = (256,256))
+
+
+classification_model = ResNet50(weights='imagenet', include_top=False, input_tensor=Input(shape=(256,256,3)))
+
+for layer in classification_model.layers:
+    layers.trainable = False
+    
+head = classification_model.output
+head = AveragePooling2D(pool_size=(4,4))(head)
+head = Flatten(name='Flatten')(head)
+head = Dense(256 , activation='relu')(head)
+head = Dropout(0.3)(head)
+head = Dense(2, activation = 'softmax')(head)
+
+model = Model(classification_model.input, head)
+model.compile(loss='categorical_crossentropy',
+              optimizer = 'adam',
+              metrics = ['accuracy'])
+
+
+earlystopping = EarlyStopping(monitor='val_loss', 
+                              mode='min', 
+                              verbose=1, 
+                              patience=15
+                             )
+checkpointer = ModelCheckpoint(filepath="clf-resnet-weights.hdf5", 
+                               verbose=1, 
+                               save_best_only=True
+                              )
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                              mode='min',
+                              verbose=1,
+                              patience=10,
+                              min_delta=0.0001,
+                              factor=0.2
+                             )
+callbacks = [checkpointer, earlystopping, reduce_lr]
+
+
+h = model.fit(train_generator, 
+              steps_per_epoch= train_generator.n // train_generator.batch_size, 
+              epochs = 50, 
+              validation_data= val_generator, 
+              validation_steps= val_generator.n // val_generator.batch_size, 
+              callbacks=[checkpointer, earlystopping])
+
+prediction = model.predict(test_generator)
+
+pred = np.argmax(prediction, axis=1)
+original = np.asarray(test_df['mask']).astype('int')
+accuracy = accuracy_score(original, pred)
+cm = confusion_matrix(original, pred)
+report = classification_report(original, pred, labels=[0,1])
