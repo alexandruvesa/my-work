c725960c19fb411db4ce146d0c3809a43356767d
Vesa Alexandru <alexandru.vesa@veoneer.com>
2021-02-06 18:49:15 +0200

    Segnet with tf zeros as structure

--- Deep Learning/Multi Task/Segnet+AttentionModule+MTL/segnet_New_method_tf_zeros.py
+++ Deep Learning/Multi Task/Segnet+AttentionModule+MTL/segnet_New_method_tf_zeros.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sat Feb  6 16:34:17 2021
+
+@author: alexandru.vesa
+"""
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Feb  4 17:58:52 2021
 
 @author: alexandru.vesa
@@ -15,7 +21,6 @@ import numpy as np
 from tensorflow.python.framework.ops import disable_eager_execution
 
 disable_eager_execution()
-
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
@@ -60,40 +65,45 @@ input_volume = tf.keras.layers.Input(shape = (256,256,3))
 
 def create_segnet(input_volume, filters):
     
-    encoders = [[0]*3]*5
-    decoders = [[0]*3] *5
+    encoders = [[tf.zeros((0))]*3]*5
     
-    encoders[0][0] = conv_block(input_volume, filters = 3, repeat = 1 ,name = 'encoder_0')
-    encoders[0][1] = conv_block(encoders[0][0], filters = 3, repeat = 1, name ='encoder_0_1' )
-    encoders[0][2] = MaxPool2D()(encoders[0][1])
+    encoders_copy = np.copy(encoders)
+    
+    decoders = [[tf.zeros((0))]*3]*5
+    
+    decoders_copy = np.copy(decoders)
+    
+    encoders_copy[0][0] = conv_block(input_volume, filters = 3, repeat = 1 ,name = 'encoder_0')
+    encoders_copy[0][1] = conv_block(encoders_copy[0][0], filters = 3, repeat = 1, name ='encoder_0_1' )
+    encoders_copy[0][2] = MaxPool2D()(encoders_copy[0][1])
     
     for i in range(4):
         print(i)
         
         if i ==0:
-            encoders[i+1][0] =  conv_block(encoders[i][2], filters = filters[i], repeat = 1,name ='encoder_1_{}'.format(i))
-            encoders[i+1][1] = conv_block(encoders[i+1][0], filters = filters[i], repeat = 1,name ='encoder_1_{}'.format(i+1))
-        encoders[i+1][2] = MaxPool2D()(encoders[i+1][1])
+            encoders_copy[i+1][0] =  conv_block(encoders_copy[i][2], filters = filters[i], repeat = 1,name ='encoder_1_{}'.format(i))
+            encoders_copy[i+1][1] = conv_block(encoders_copy[i+1][0], filters = filters[i], repeat = 1,name ='encoder_1_{}'.format(i+1))
+            encoders_copy[i+1][2] = MaxPool2D()(encoders_copy[i+1][1])
         
         
         if i>0:
-            encoders[i+1][0] = conv_block(encoders[i][2], filters = filters[i+1], repeat = 1,name ='encoder_{}_{}'.format(i+1,i))
-            encoders[i+1][1] = conv_block(encoders[i+1][0], filters = filters[i], repeat = 1,name ='encoder_{}_{}'.format(i+1, i+1))
-        encoders[i+1][2] = MaxPool2D()(encoders[i+1][1])
+            encoders_copy[i+1][0] = conv_block(encoders_copy[i][2], filters = filters[i+1], repeat = 1,name ='encoder_{}_{}'.format(i+1,i))
+            encoders_copy[i+1][1] = conv_block(encoders_copy[i+1][0], filters = filters[i+1], repeat = 1,name ='encoder_{}_{}'.format(i+1, i+1))
+        encoders_copy[i+1][2] = MaxPool2D()(encoders_copy[i+1][1])
         
     for i in range(5):
         if i ==0:
-            decoders[i][0] = UpSampling2D()(encoders[4][2])
-            decoders[i][1] = conv_block(decoders[i][0], filters = filters[-1],repeat = 1 , name ='decoder_{}'.format(i))
-            decoders[i][2] = conv_block(decoders[i][1], filters = filters[-1],repeat = 1 , name ='decoder_{}_{}'.format(i,i+1))
+            decoders_copy[i][0] = UpSampling2D()(encoders_copy[4][2])
+            decoders_copy[i][1] = conv_block(decoders_copy[i][0], filters = filters[-1],repeat = 1 , name ='decoder_{}'.format(i))
+            decoders_copy[i][2] = conv_block(decoders_copy[i][1], filters = filters[-1],repeat = 1 , name ='decoder_{}_{}'.format(i,i+1))
 
-        # if i>0:
-        #     print(i)
-        #     decoders[i][0] = UpSampling2D()(decoders[i-1][2])
-        #     decoders[i][1] = concatenate([encoders[-i+4][1],decoders[i][0]])
-        #     decoders[i][2] = conv_block(decoders[i+1][1], filters = filters[-i-4],repeat = 1, name = 'decoder_{}_{}'.format(i,i+1))
+        if i>0:
+            print(i)
+            decoders_copy[i][0] = UpSampling2D()(decoders_copy[i-1][2])
+            decoders_copy[i][1] = concatenate([encoders_copy[-i+4][1],decoders_copy[i][0]])
+            decoders_copy[i][2] = conv_block(decoders_copy[i][1], filters = filters[-i-1],repeat = 1, name = 'decoder_{}_{}'.format(i,i+1))
     
-    model = tf.keras.models.Model(inputs = input_volume, outputs = decoders)
+    model = tf.keras.models.Model(inputs = input_volume, outputs = decoders_copy.tolist())
 
     model.save('segnet_method_list.h5')
     
